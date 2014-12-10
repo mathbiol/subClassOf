64900b036ead0879633c9b4ba373fc33d59ffb0e
David Robbins <robbins.david@gmail.com>
2014-12-10 10:02:34 -0600

    Initial barebones app

---
+++ prototypical.js
@@ -0,0 +1,17 @@
+console.log("Hello from Prototypical");
+
+function Thing(initialStuff) {
+    var keys = Object.keys(initialStuff);
+    for (var i = keys.length - 1; i >= 0; i--) {
+        this[keys[i]] = initialStuff[keys[i]]
+    };
+}
+
+Thing.prototype.get = function get(key) {
+    return this[key];
+}
+
+Thing.prototype.set = function set(key, value) {
+    this[key] = value;
+    return this;
+}
