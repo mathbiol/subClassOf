3166823664534cdbdebea5fae9986e69af3f9345
Jonas Almeida <jalmeida@mathbiol.org>
2014-12-10 17:27:26 -0600

    subClassOf

--- subClassOf.js
+++ subClassOf.js
@@ -1,1 +1,19 @@
 console.log('subClassOf.js')
+
+Object.prototype.subClassOf=function(x){
+    console.log('subClassOf',x)
+    if(typeof(x)=='object'){
+        var k = Object.getOwnPropertyNames(x)
+        for(i=0;i<k.length;i++){
+            if(!this[k[i]]){this[k[i]]=x[k[i]]}
+        }
+    }else{
+        throw("will load parent from URL, check back later")
+    }
+    
+    return this
+}
+
+// example:
+// a={b:9}
+// a.subClassOf({c:10})
