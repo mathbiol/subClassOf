6106b7df56b162cf3572c554f57290ac732c37a9
David Robbins <robbins.david@gmail.com>
2014-12-10 10:28:58 -0600

    Added initial prototype demo

--- prototypical.js
+++ prototypical.js
@@ -8,10 +8,26 @@ function Thing(initialStuff) {
 }
 
 Thing.prototype.get = function get(key) {
-    if (this.hasOwnProperty(key)) return this[key];
+    if (this.hasOwnProperty(key)) {
+        return this[key];
+    }
+    else if (this.hasOwnProperty("prototype")) {
+        var req = new XMLHttpRequest();
+        req.open("GET", this.prototype, false);
+        req.send(null);
+        return JSON.parse(req.responseText)[key];
+    }
 }
 
 Thing.prototype.set = function set(key, value) {
     this[key] = value;
     return this;
+}
+
+window.prototypical = {
+    Thing: Thing,
+    demo: function () {
+        var dog = { name: "Sam", breed: "Mutt", prototype: "https://48067914bb3d7935906839bc04226b8b5f55d44b-www.googledrive.com/host/0Bzu4cytkv4B8aXZ0UUpiXzkzclE/boxer.json" };
+        return new Thing(dog);
+    }
 }
