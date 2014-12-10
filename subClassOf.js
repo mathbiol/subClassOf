1ce249b928b148628c1f8d081f1420e2eecdc097
Jonas Almeida <jalmeida@mathbiol.org>
2014-12-10 17:57:04 -0600

    ready for StefansCar

--- subClassOf.js
+++ subClassOf.js
@@ -1,19 +1,33 @@
 console.log('subClassOf.js')
 
 Object.prototype.subClassOf=function(x){
-    console.log('subClassOf',x)
-    if(typeof(x)=='object'){
+    if(typeof(x)=='object'){ // prototype is an object in the local scope
         var k = Object.getOwnPropertyNames(x)
         for(i=0;i<k.length;i++){
             if(!this[k[i]]){this[k[i]]=x[k[i]]}
         }
-    }else{
-        throw("will load parent from URL, check back later")
+        console.log(this,'subClassOf',x)
+    }else{ // assume x is the URL of prototype object and go get it
+        var that = this
+        var load = function(url){ // XMLHttpRequest
+            var r = new XMLHttpRequest();
+            r.onload=function(){
+                var x = eval("("+this.responseText+")")
+                that.subClassOf(x)
+            }; 
+            r.open("GET",url,true);
+            r.send();
+            return this
+        }
+        load(x)
     }
-    
     return this
 }
 
-// example:
+// examples:
+//
 // a={b:9}
 // a.subClassOf({c:10})
+//
+// car={color:"blue"}
+// car.subClassOf("https://0857f9879749e82d493945f8a805968a7c031889-www.googledrive.com/host/0BwwZEXS3GesiTjlHSmlOcEJaeDA/Prototypical/StefansCar.json")
